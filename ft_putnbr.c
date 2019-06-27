2124ca3feea114be3e5dcd0187c5f053a9f7c676
Benjot <benjaminmoreau89000@gmail.com>
2019-06-27 15:50:05 +0200

    putnbrarg

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,24 +1,55 @@
-void	ft_putchar(char c);
-
-void	ft_putnbr(int nb)
+#include <unistd.h>
+#include <stdio.h>
+void ft_putchar(char c)
 {
-	unsigned int n;
-	if (nb < 0)
+	write(1,&c,1);
+}
+
+void ft_putnbr(int n)
+{
+	unsigned int nb;
+	nb = n;
+	if (n < 0)
 	{
+		nb = -n;
 		ft_putchar('-');
-		n = nb * (-1);
 	}
-	else
+	if (nb >= 10)
 	{
-		n = nb;
+		ft_putnbr(nb/10);
+		ft_putnbr(nb%10);
 	}
-	if (n > 9)
+	if (nb <10)
+		ft_putchar(nb%10 + 48);
+}
+
+void ft_convert_arg(char *str)
+{
+	int i;
+	int j;
+	int neg;
+	i=0;
+	j=0;
+	neg = 0 ;
+	if (str[i] == '-')
+		neg =1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] != '\0')
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		j=(j*10)+(str[i]-'0');
+		i++;
 	}
-	if (n <= 9)
-	{
-		ft_putchar((nb % 10) + 48);
-	}
+	if (neg == 1)
+		j = -j;
+	if (str == "-2147483648")
+		j = -2147483648;
+	ft_putnbr(j);
+}
+
+int main(int argc,char ** argv)
+{
+	(void)argc;
+	ft_convert_arg(argv[1]);
+	return (0);
 }
